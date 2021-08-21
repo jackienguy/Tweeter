e7308ae694b3d27ada3516e2ded205b77f298311
Your Name <you@example.com>
2021-08-21 13:53:05 -0600

    adding views signin, register, discover, profile and feed

---
+++ tweeterproject/src/views/SignIn.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <form @submit.prevent="signin">
+            <h1>Log into Tweeter</h1>
+            <input type="text" placeholder="Username or Email" v-model="user"/>
+            <input type="password" placeholder="password" v-model="password"/>
+
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

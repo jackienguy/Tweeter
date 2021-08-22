c023b5ded1c8fdf5dd007d9e1d5be3816b5a9c00
Your Name <you@example.com>
2021-08-22 14:00:30 -0600

    using Vuetify to create login form for /views/SignIn.vue

--- tweeterproject/src/App.vue
+++ tweeterproject/src/App.vue
@@ -1,35 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/SignIn">Sign In</router-link> |
-      <router-link to="/">Home</router-link> |
-      <router-link to="/Profile">Profile</router-link> |
-      <router-link to="/Feed">Feed</router-link> |
-      <router-link to="/Discovery">Discovery</router-link> |
-    </div>
-    <router-view/>
+    <SignIn/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+<script>
+import SignIn from './views/SignIn.vue'
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    export default {
+        name: "App",
+        components: {
+          SignIn
+        }
+    }
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+
--- tweeterproject/src/views/SignIn.vue
+++ tweeterproject/src/views/SignIn.vue
@@ -1,20 +1,76 @@
 <template>
     <div>
-        <form @submit.prevent="signin">
-            <h1>Log into Tweeter</h1>
-            <input type="text" placeholder="Username or Email" v-model="user"/>
-            <input type="password" placeholder="password" v-model="password"/>
-            <button @click="login">Sign In</button>
-        </form>
+    <!-- Vuetify Form for Signin -->
+    <form>
+        <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            label="Name"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+        ></v-text-field>
+        <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+        ></v-text-field>
+        <v-text-field
+            v-model="password"
+            :error-messages="passwordErrors"
+            label="Password"
+            required
+            @input="$v.password.$touch()"
+            @blur="$v.password.$touch()"
+        ></v-text-field>
+        <v-btn class="mr-4" @click="login">Login</v-btn>
+    </form>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
 import cookies from 'vue-cookies';
+import { validationMixin } from 'vuelidate';
+import { required, email } from 'vuelidate/lib/validators';
 
     export default {
         name: "SignIn",
+        mixins: [validationMixin],
+        validations: {
+            name: { required },
+            email: { required, email },
+            password:  { required }
+        },
+        data: () => ({
+            name: '',
+            email: '', 
+            password: '',
+        }),
+        computed: {
+            nameErrors () {
+                const errors = []
+                if (!this.$v.name.$dirty) return errors
+                !this.$v.name.required && errors.push('Name is required.')
+                return errors
+            },
+            emailErrors () {
+                const errors = []
+                if (!this.$v.email.$dirty) return errors
+                !this.$v.email.email && errors.push('Must be valid e-mail')
+                !this.$v.email.required && errors.push('E-mail is required')
+                return errors
+            },
+            passwordErrors () {
+                const errors = []
+                if (!this.$v.password.$dirty) return errors
+                !this.$v.password.required && errors.push('Password is required.')
+                return errors
+            }
+        },
         methods: {
             login () {
                 axios.request({
@@ -22,11 +78,8 @@ import cookies from 'vue-cookies';
                     method: "POST",
                     headers: {
                         'Content-Type': 'application/json',
+                         'X-Api-Key': process.env.VUE_APP_API_KEY
                     },
-                    data: {
-                        "email": "CindyLou@suess.com",
-                        "password": "IStoleChristmas"
-                    }
                 }).then((response) => {
                     cookies.set('loginToken', response.data.token);
                     console.log(response.data.token);
